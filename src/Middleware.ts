dac44ca1c7483af54befc9b22d8807d9fece1c23
Brandon Konkle <brandon@ecliptic.io>
2019-01-15 22:57:04 -0700

    Type fixes

--- src/Middleware.ts
+++ src/Middleware.ts
@@ -1,4 +1,4 @@
-import {Request, Response, NextFunction, RequestHandler} from 'express'
+import {Request, Response, NextFunction} from 'express'
 
 // tslint:disable-next-line no-any
 export type PromiseMiddleware = (req: Request, res: Response) => Promise<any>
